595c9d7a9e8498fe02a79db8848cb3ae2c2daccd
juanjo <juanjpina@gmail.com>
2021-08-26 18:25:42 +0200

    26-08-01

--- src/views/users/loginView.php
+++ src/views/users/loginView.php
@@ -1,39 +1,47 @@
 <?php get_header('Se connecter', 'login') ?>
 
-<div class="menu-login">
+<div class="menu-login column">
 	<div class="login">
 		<form action="" method="post">
+			<ul>
+				<li>
 
-			<div class="mail">
-				<p class="parrafo-compte">Déjà une compte</p>
-				<?php $error = checkField('login', 'Votre email est vide.'); ?>
-				<input type="email" class="email<?= $error['class']; ?>" id="email" placeholder="Adresse email" name="login" value="<?= valueField('login'); ?>">
+					<div class="mail">
+						<p class="parrafo-compte">Déjà une compte</p>
+						<?php $error = checkField('login', 'Votre email est vide.'); ?>
+						<input type="email" class=" input email<?= $error['class']; ?>" id="email" placeholder="Adresse email" name="login" value="<?= valueField('login'); ?>">
 
-				<p class="message"> <?= $error['message']; ?> </p>
-			</div>
+						<p class="message"> <?= $error['message']; ?> </p>
+					</div>
+				</li>
+				<li>
 
-			<div class="div-password">
-				<?php $error = checkField('password', 'Votre mot de passe est vide.'); ?>
-				<input type="password" class="password<?= $error['class']; ?>" id="password" placeholder="Mot de passe" name="password" value='Mot de passe'>
-				<a href="">
-					<p class="parrafo-mot">Mot de passe oublié?</p>
-				</a>
-				<?= $error['message']; ?>
+					<div class="div-password">
+						<?php $error = checkField('password', 'Votre mot de passe est vide.'); ?>
+						<input type="password" class=" input password<?= $error['class']; ?>" id="password" placeholder="Mot de passe" name="password" value='Mot de passe'>
+						<a href="">
+							<p class="parrafo-mot">Mot de passe oublié?</p>
+						</a>
+						<?= $error['message']; ?>
 
-			</div>
-			<div class="submit">
-				<button class="button" type="submit">
-					<p>Se connecter</p>
-				</button>
-			</div>
+					</div>
+				</li>
+				<li>
+					<a href="#" class="column">
+						<div class="button">
+							<intut class="" type="submit">Se connecter</intut>
+						</div>
+					</a>
+				</li>
+			</ul>
 		</form>
 
 	</div>
-	<div class="compte">
-		<a class="creercompte" href="<?= $router->generate('newlogin'); ?>">
+	<a class="creercompte" href="<?= $router->generate('newlogin'); ?>">
+		<div class="compte">
 			<p>Créer une compte</p>
-		</a>
+		</div>
+	</a>
 
-	</div>
 </div>
 <?php get_footer('login') ?>
