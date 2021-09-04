20266576bc325f546fd94132682874275bb60f36
juanjo <juanjpina@gmail.com>
2021-09-04 08:44:39 +0200

    04-09-01

--- src/controllers/statistics/admin_fuelStatisticsController.php
+++ src/controllers/statistics/admin_fuelStatisticsController.php
@@ -1,4 +1,14 @@
 <?php
 
+if (!empty($_SESSION['car']['id_car'])) {
+    $data = [
+        ':id_car' => $_SESSION['car']['id_car'],
+    ];
 
-$sql = "";
+    $sql = "SELECT fuel.km, MAX(invfuel.km), SUM(invfuel.total) FROM fuel, invfuel WHERE invfuel.id_car= :id_car AND fuel.km<=invfuel.km;";
+
+    $request = $db->prepare($sql);
+    $request->execute($data);
+    $result = $request->fetchAll(PDO::FETCH_ASSOC);
+    dump($result);
+}
