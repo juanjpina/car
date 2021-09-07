e30865ad96e70893c16063b028dae035413497c4
juanjo <juanjpina@gmail.com>
2021-09-07 20:22:27 +0200

    07-09-01

--- src/views/pages/admin_indexView.php
+++ src/views/pages/admin_indexView.php
@@ -4,8 +4,7 @@ Bonjour <?= $_SESSION['auth']['email']; ?>
 
 <div class="accueil">
     <div class="texto">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
+        <p>Bienvenue sur notre site. Ici vous pourrez gérer vos véhicules. Vous pouvez ajouter ou supprimer des véhicules de votre liste comme vous le souhaitez. Pour que vous comprenez mieux, vous pourrez visualiser les dernières interventions réalisées sur votre (ou vos si vous en possédez plusieurs) véhicule, mais aussi celles qui sont à venir. Non seulement, vous pourrez aussi observer vos dépenses annuelles et les comparer aux années précédentes. Il suffira de remplir régulièrement les informations nécessaires, telles que les frais d’entretien, les factures, le kilométrage, etc.</p>
         <div class="continuer">
             <a href="<?= $router->generate('addnewcar'); ?>">Vous pouvez continuer, merci</a>
         </div>
--- src/views/users/indexView.php
+++ src/views/users/indexView.php
@@ -1,5 +1,147 @@
-<?php get_header('home', 'login'); ?>
+<!-- <?php get_header('home', 'login'); ?> -->
+<style>
+    .card-box {
+        width: 200px;
+        height: 250px;
+        position: relative;
+        perspective: 1000px;
+        margin-right: 2em;
+    }
+
+    .card-box:hover .card {
+        transform: rotateY(180deg);
+    }
+
+    .card {
+        transform-style: preserve-3d;
+        transition: all 1s linear;
+    }
+
+    .front {
+        position: absolute;
+        backface-visibility: hidden;
+    }
+
+    .front.back {
+        transform: rotateY(180deg);
+    }
+
+    .container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding-top: 1.2em;
+    }
+
+    .wraper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    p {
+        text-align: center;
+    }
+
+    .containerMain {
+        margin-top: 10em;
+    }
+</style>
+<div class="wraper">
+
+    <div class="containerMain">
+
+        <div class="container">
+            <div class="card-box">
+                <div class="card">
+                    <div class="front">
+                        <img src="/proyectocar/car/src/assets/images/index_alert.png" width="200px" height="250px" alt="">
+                    </div>
+                    <div class="front back">
+                        <img src="https://placeimg.com/200/250/animals" width="200" height="250px">
+                    </div>
+                </div>
+            </div>
+            <div class="card-box">
+                <div class="card">
+
+                    <div class="front">
+                        <img src="https://placeimg.com/200/250/tech" width="200" height="250px">
+                    </div>
+
+                    <div class="front back">
+                        <img src="/proyectocar/car/src/assets/images/statistics_index.png" width="200px" height="250px" alt="">
+                    </div>
+
+                </div>
+            </div>
+            <div class="card-box">
+                <div class="card">
+                    <div class="front">
+                        <img src="/proyectocar/car/src/assets/images/history_index.png" width="200px" height="250px" alt="">
+                    </div>
+                    <div class="front back">
+                        <img src="https://placeimg.com/200/250/animals" width="200" height="250px">
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="container">
+            <div class="card-box">
+                <div class="card">
+                    <div class="front">
+                        <img src="https://placeimg.com/200/250/tech" width="200" height="250px">
+                    </div>
+                    <div class="front back">
+                        <img src="/proyectocar/car/src/assets/images/invoice_index.png" width="200px" height="250px" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-box">
+                <div class="card">
+                    <div class="front">
+                        <img src="/proyectocar/car/src/assets/images/index_car.png" width="200px" height="250px" alt="">
+                    </div>
+                    <div class="front back">
+                        <img src="https://placeimg.com/200/250/tech" width="200" height="250px">
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-box">
+                <div class="card">
+                    <div class="front">
+                        <img src="https://placeimg.com/200/250/tech" width="200" height="250px">
+                    </div>
+                    <div class="front back">
+                        <img src="https://placeimg.com/200/250/animals" width="200" height="250px">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
 
 
-<?php get_footer('login');
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer('login'); ?>
