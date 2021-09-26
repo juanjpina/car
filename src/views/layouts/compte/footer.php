6d03e10484bf6290c3d6b466bfeccb2a17bf317e
juanjo <juanjpina@gmail.com>
2021-09-26 14:31:59 +0200

    26-09-07

--- src/views/layouts/compte/footer.php
+++ src/views/layouts/compte/footer.php
@@ -1,6 +1,30 @@
 </main>
-</script>
-
 </body>
+
+
+
+
+<footer>
+    <nav>
+
+        <ul>
+            <li>
+                <div>
+                    <p>
+                        Copyright ©2021
+                </div>
+                </p>
+            </li>
+            <li>
+                <div><a href="mailto:hege@example.com">hege@example.com</a></div>
+            </li>
+            <li>
+                <div></div>
+            </li>
+        </ul>
+
+    </nav>
+</footer>
+
 
 </html>
--- src/views/layouts/login/footer.php
+++ src/views/layouts/login/footer.php
@@ -1,4 +1,30 @@
 	</main>
 	</body>
 
+
+
+
+	<footer>
+		<nav>
+
+			<ul>
+				<li>
+					<div>
+						<p>
+							Copyright ©2021
+					</div>
+					</p>
+				</li>
+				<li>
+					<div><a href="mailto:hege@example.com">hege@example.com</a></div>
+				</li>
+				<li>
+					<div></div>
+				</li>
+			</ul>
+
+		</nav>
+	</footer>
+
+
 	</html>
