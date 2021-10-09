96c7b80a244f6f80e7f02f47a2404eda0c55038f
juanjo <juanjpina@gmail.com>
2021-10-09 12:09:36 +0200

    09-10-02

--- src/views/users/pswForgetView.php
+++ src/views/users/pswForgetView.php
@@ -1,26 +1,26 @@
 <?php get_header('pawforget', 'login'); ?>
-<section>
-    <div class="pswforget">
-        <div class="container column">
-            <form action="" method="post">
-                <div class="column">
-                    <label for="mail">e-mail</label>
-                    <input type="email" name='mail' placeholder="example@example.com" value="">
-                </div>
-                <div class="column">
-                    <input type='hidden' name='ok' value='ok'>
-                    <button class="button" type="submit">Sauvegarder</button>
-                </div>
-            </form>
-            <p><?= $value; ?></p>
-            <div>
-                <a href="<?= $router->generate('login'); ?>">
-                    <div class="button">
-                        <p>Login</p>
-                    </div>
-                </a>
+<section class="column">
+    <div class="pswforget column">
+        <!-- <div class="container column"> -->
+        <form action="" method="post">
+            <div class="column">
+                <label for="mail">e-mail</label>
+                <input type="email" name='mail' placeholder="example@example.com" value="">
             </div>
+            <div class="column">
+                <input type='hidden' name='ok' value='ok'>
+                <button class="button" type="submit">Sauvegarder</button>
+            </div>
+        </form>
+        <p><?= $value; ?></p>
+        <div>
+            <a href="<?= $router->generate('login'); ?>">
+                <div class="button">
+                    <p>Login</p>
+                </div>
+            </a>
         </div>
     </div>
+    <!-- </div> -->
 </section>
 <? get_footer('login'); ?>
