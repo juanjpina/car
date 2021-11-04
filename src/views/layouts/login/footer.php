84e6d46f88826eb774eaf58527b4e319c7376c0c
= <=>
2021-11-04 11:23:54 +0100

    04/11/02

--- src/views/layouts/login/footer.php
+++ src/views/layouts/login/footer.php
@@ -1,30 +1,38 @@
-	</main>
-	<footer>
-		<nav>
-			<ul>
-				<li>
-					<div>
-						<p>
-							&copy; Copyright <?= date('Y'); ?>
-						</p>
-					</div>
-				</li>
-				<li>
-					<div>
-						<p>
-							<a href="http://localhost/car/mention">mention légale</a>
-						</p>
-					</div>
-				</li>
-				<li>
-					<div><a href="mailto:hege@example.com">hege@example.com</a></div>
-				</li>
-			</ul>
-		</nav>
-	</footer>
+</main>
+<footer>
+    <nav>
+        <ul>
+            <div class="social row">
+                <li>
+                    <a href="https://www.facebook.com"><img src="/car/src/assets/images/facebook.png" width="40" height="40"></a>
+                </li>
+                <li>
+                    <a href="https://www.twitter.com"><img src="/car/src/assets/images/twitter.png" width="40" height="40"></a>
+                    
+                </li>
+            </div>
+            <div class="law">
+                <li>
+                    <div><a href="mailto:hege@example.com">hege@example.com</a></div>
+                </li>
+                <li>
+                    <div>
+                        <p>
+                            <a href="http://localhost/car/mention">mention légale</a>
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <p>
+                            &copy; Copyright <?= date('Y'); ?>
+                        </p>
+                    </div>
+                </li>
+            </div>
+        </ul>
+    </nav>
+</footer>
+</body>
 
-
-	</body>
-
-
-	</html>
+</html>
