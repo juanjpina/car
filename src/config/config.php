50c4192de6573b736912aa5ba61fe0c342f40111
juanjpina <juanjpina@gmail.com>
2023-02-13 11:46:38 +0100

    config

--- src/config/config.php
+++ src/config/config.php
@@ -4,9 +4,9 @@ define('DEBUG', true);
 
 // Database informations
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'car');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_NAME', 'u158371929_car');
+define('DB_USER', 'u158371929_dbuser');
+define('DB_PASSWORD', '23Hacer151*');
 
-define('BASEPATH', '/car');
+define('BASEPATH', '');
 define('ROOT', dirname(__DIR__));
